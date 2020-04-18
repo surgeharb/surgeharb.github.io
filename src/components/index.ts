86f8d6ce7a70245c157c6d8e462be8794e9ab835
Serge Harb <me@sergeharb.com>
2020-04-18 13:32:08 +0300

    add build for master branch

--- src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './navbar';
-export * from './intro';
-export * from './content';
-export * from './social';
-export * from './footer';
--- src/data/social-media.ts
+++
@@ -1,5 +0,0 @@
-export const SocialMedia = [
-  { name: 'linkedin', link: 'https://www.linkedin.com/in/serge-harb' },
-  { name: 'twitter', link: 'https://twitter.com/surgeharb' },
-  { name: 'github', link: 'https://github.com/surgeharb' }
-];
