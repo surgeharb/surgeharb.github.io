2b73220f7261b562ec8f43beb53ac7a3b92129ad
Serge Harb <me@sergeharb.com>
2023-05-31 12:32:33 +0200

    remove unused files

--- components/ui/compound/tabs/index.tsx
+++ components/ui/compound/tabs/index.tsx
@@ -1,3 +1,2 @@
 export * from './companies';
 export * from './projects';
-export * from './skills';
--- data/companies.ts
+++ data/companies.ts
@@ -1,21 +1,5 @@
 import { Company } from '@/models/Company';
 
-// <ContentRow
-//       url="/images/preply.png"
-//       rightText="Preply, Barcelona 🇪🇸"
-//       leftText="Senior Software Engineer"
-//     />
-//     <ContentRow
-//       url="/images/tf961.jpeg"
-//       rightText="TF961, Lebanon 🇱🇧"
-//       leftText="Senior Software Engineer"
-//     />
-//     <ContentRow
-//       url="/images/fibler.jpeg"
-//       rightText="Fibler, Lebanon 🇱🇧"
-//       leftText="Software Engineer"
-//     />
-
 export const companies: Company[] = [
   {
     link: 'https://preply.com',
