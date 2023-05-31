f21f18bebcfb8a0373463ba86f05de46a9095bca
Serge Harb <me@sergeharb.com>
2023-05-31 12:57:28 +0200

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 import { ThemeManager } from '@/components/theme-manager';
 import { cn } from '@/lib/utils';
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={cn(inter.className, isDarkMode && 'dark')}>
         {children}
+        <Analytics />
         <ThemeManager showStickyButton />
       </body>
     </html>
