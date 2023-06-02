caf7ef5e4b69d247dc387276f7ba1d942bdc78a7
Serge Harb <me@sergeharb.com>
2023-06-02 15:05:27 +0200

    Update metadata.ts

--- data/metadata.ts
+++ data/metadata.ts
@@ -1,4 +1,4 @@
-export const title = 'Serge Harb - Full Stack Software Engineer';
+export const title = 'Serge Harb - Software Engineer';
 
 export const description =
   'Serge Harb, Full Stack Software Engineer with a demonstrated history of working in the software industry. Mainly expert in artificial intelligence, modern web and mobile apps development.';
