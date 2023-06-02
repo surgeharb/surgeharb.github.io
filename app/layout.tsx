a1f439e3aed314f04de737cc0debd63ddc5e384d
Serge Harb <me@sergeharb.com>
2023-06-02 12:04:18 +0200

    improve SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { ThemeManager } from '@/components/theme-manager';
-import { title, description, website } from '@/data/metadata';
+import { title, description, website, twitterHandle } from '@/data/metadata';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -11,22 +11,12 @@ export const metadata = {
   title,
   description,
   url: website,
+  themeColor: '#1e293b',
   twitter: {
     card: 'summary_large_image',
     title,
     description,
-    creator: '@surgeharb',
-    images: [`${website}/icons/twitter-card-image.png`],
-  },
-  manifest: '/icons/site.webmanifest',
-  themeColor: '#1e293b',
-  icons: {
-    apple: '/icons/apple-touch-icon.png',
-    favicon: [
-      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-    ],
-    shortcut: '/icons/favicon.ico',
+    creator: twitterHandle,
   },
 };
 
--- data/metadata.ts
+++ data/metadata.ts
@@ -1,6 +1,8 @@
 export const title = 'Serge Harb - Full Stack Software Engineer';
 
 export const description =
-  'Serge Harb, Full Stack Software Engineer with a demonstrated history of working in the software industry. Mainly expert in backend and frontend web development, as well as modern mobile and web apps development.';
+  'Serge Harb, Full Stack Software Engineer with a demonstrated history of working in the software industry. Mainly expert in artificial intelligence, modern web and mobile apps development.';
 
 export const website = 'https://sergeharb.com';
+
+export const twitterHandle = '@surgeharb';
