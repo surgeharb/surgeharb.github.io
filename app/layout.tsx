c45c9ec4795a2cbcaf6153b145b7c63de153b79c
Serge Harb <me@sergeharb.com>
2023-06-02 10:36:23 +0200

    SEO metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,32 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { ThemeManager } from '@/components/theme-manager';
+import { title, description, website } from '@/data/metadata';
 import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Serge Harb - Full Stack Software Engineer',
-  description:
-    'Serge Harb, Full Stack Software Engineer with a demonstrated history of working in the software industry. Mainly expert in backend and frontend web development, as well as modern mobile and web apps development.',
+  title,
+  description,
+  url: website,
+  twitter: {
+    card: 'summary_large_image',
+    title,
+    description,
+    creator: '@surgeharb',
+    images: [`${website}/icons/twitter-card-image.png`],
+  },
+  manifest: '/icons/site.webmanifest',
+  themeColor: '#1e293b',
+  icons: {
+    apple: '/icons/apple-touch-icon.png',
+    favicon: [
+      { url: '/icons/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/icons/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    shortcut: '/icons/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
---
+++ data/metadata.ts
@@ -0,0 +1,6 @@
+export const title = 'Serge Harb - Full Stack Software Engineer';
+
+export const description =
+  'Serge Harb, Full Stack Software Engineer with a demonstrated history of working in the software industry. Mainly expert in backend and frontend web development, as well as modern mobile and web apps development.';
+
+export const website = 'https://sergeharb.com';
